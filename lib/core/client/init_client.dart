087189c3f1759eeb66203a4a342e4d2b4f841ab6
YorqinBek7 <yorqinbekyuldashev33@gmail.com>
2026-02-16 03:31:26 +0500

    fixed bug

--- lib/core/client/init_client.dart
+++ lib/core/client/init_client.dart
@@ -5,20 +5,20 @@ import 'package:testbor/core/link/auth_link.dart';
 import 'package:testbor/core/route/router.dart';
 import 'package:testbor/service_locator.dart';
 
-AuthLink _createAuthLink() {
-  return AuthLink(sl<FlutterSecureStorage>(), () async {
-    await AppRouter.router.pushReplacement(AppRouter.auth);
-  });
-}
-
 Future<Client> initClient() async {
   final httpLink = HttpLink('https://api.testbor.com/auth/graphql');
-  final link = Link.from([_createAuthLink(), httpLink]);
+  final authLink = AuthLink(sl<FlutterSecureStorage>(), () async {
+    await AppRouter.router.pushReplacement(AppRouter.auth);
+  });
+  final link = Link.from([authLink, httpLink]);
   return Client(link: link);
 }
 
 Future<Client> initProfileClient() async {
   final httpLink = HttpLink('https://api.testbor.com/profile/graphql');
-  final link = Link.from([_createAuthLink(), httpLink]);
+  final authLink = AuthLink(sl<FlutterSecureStorage>(), () async {
+    await AppRouter.router.pushReplacement(AppRouter.auth);
+  });
+  final link = Link.from([authLink, httpLink]);
   return Client(link: link);
 }
